8674e64d0ba8803d3f7beb5db0a6ca8e886bfff3
Tim Zababurin <raccoontd@mail.ru>
2022-02-04 19:44:04 +0300

    gallery-mod4(scrollY_working)

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,8 @@ const scrollContainer = document.querySelector("body");
 const scrollGallery = document.querySelector(".gallery-container");
 let offsetY = 0;
 let offsetX = 0;
-let scroll = true;
+let scrollBeforeGallery = true;
+let scrollAfterGallery = false;
 const deltaV = 0.4;
 let deltaH = 0;
 let deltaW = 0;
@@ -18,18 +19,21 @@ window.addEventListener('load', () => {
     
         evt.preventDefault();
         
-        if (scroll) { offsetX -= evt.deltaY * deltaV;  }
+        if (scrollBeforeGallery || scrollAfterGallery) { offsetX -= evt.deltaY * deltaV;  }
         else offsetY -= evt.deltaY * deltaV;
         
         if (offsetX > 0) offsetX = 0;
         if (offsetX < -deltaW) offsetX = -deltaW;
-        // if (offsetX < -2850 && offsetX > -2890) scroll = false;
+        if (offsetX < -2880 && scrollBeforeGallery) { scrollBeforeGallery = false; offsetX = -2880 };
+        if (offsetX > -2880 && scrollAfterGallery) { scrollAfterGallery = false; offsetX = -2880 };
 
-        if (offsetY > 0) offsetY = 0;
-        if (offsetY < -deltaH) offsetY = -deltaH;
+        if (offsetY > 0 && !scrollBeforeGallery) {offsetY = 0; scrollBeforeGallery = true }
+        if (offsetY < -deltaH && !scrollAfterGallery) { offsetY = -deltaH; scrollAfterGallery = true }
 
         scrollContainer.style.transform = `translateX(${offsetX}px)`;
-        console.log(offsetX, scroll);
+        scrollGallery.style.transform = `translateY(${offsetY}px)`;
+
+        console.log(offsetX, offsetY, scrollBeforeGallery, scrollAfterGallery);
     });
 
     // scrollGallery.addEventListener('wheel', (evt) => {
