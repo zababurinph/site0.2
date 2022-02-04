b1627ac802b17acd4f03e6ea37f29429ded03108
Tim Zababurin <raccoontd@mail.ru>
2022-02-04 11:30:28 +0300

    gallery-mod3

--- src/js/main.js
+++ src/js/main.js
@@ -1,46 +1,46 @@
 import './parts/about.js';
 import '../scss/main.scss';
 
-const scrollContainer = document.querySelector("main");
+const scrollContainer = document.querySelector("body");
 const scrollGallery = document.querySelector(".gallery-container");
 let offsetY = 0;
 let offsetX = 0;
 let scroll = true;
-const deltaV = 0.6;
+const deltaV = 0.4;
 let deltaH = 0;
+let deltaW = 0;
 
 window.addEventListener('load', () => {
-    let deltaW = scrollContainer.scrollWidth - document.documentElement.clientWidth;
-
+    
     scrollContainer.addEventListener('wheel', (evt) => {
+        deltaW = scrollContainer.scrollWidth - document.documentElement.clientWidth;
         deltaH = scrollGallery.offsetHeight - document.documentElement.clientHeight;
     
         evt.preventDefault();
         
-        if (scroll) {
-            // scrollContainer.scrollLeft += evt.deltaY * deltaV;
-            offsetX -= evt.deltaY * deltaV;
-        }
-        // scrollContainer.scrollLeft += evt.deltaX * deltaV;
-        // offsetX -= evt.deltaX * deltaV;
-        scrollContainer.style.transform = `translateX(${offsetX}px)`;
-
+        if (scroll) { offsetX -= evt.deltaY * deltaV;  }
+        else offsetY -= evt.deltaY * deltaV;
+        
         if (offsetX > 0) offsetX = 0;
         if (offsetX < -deltaW) offsetX = -deltaW;
-        // if (offsetX < -2845 && offsetX > -2890) scroll = false;
+        // if (offsetX < -2850 && offsetX > -2890) scroll = false;
 
-        // console.log(offsetX, scroll)
+        if (offsetY > 0) offsetY = 0;
+        if (offsetY < -deltaH) offsetY = -deltaH;
+
+        scrollContainer.style.transform = `translateX(${offsetX}px)`;
+        console.log(offsetX, scroll);
     });
 
-    scrollGallery.addEventListener('wheel', (evt) => {
-        evt.preventDefault();
-        offsetY -= evt.deltaY * deltaV;
+    // scrollGallery.addEventListener('wheel', (evt) => {
+    //     evt.preventDefault();
+    //     offsetY -= evt.deltaY * deltaV;
         
-        if (offsetY > 0) { offsetY = 0; scroll = true }
-        else if (offsetY < -deltaH) { offsetY = -deltaH; scroll = true }
-        else scroll = false;
+    //     if (offsetY > 0) { offsetY = 0; scroll = true }
+    //     else if (offsetY < -deltaH) { offsetY = -deltaH; scroll = true }
+    //     else scroll = false;
 
-        scrollGallery.style.transform = `translateY(${offsetY}px)`;
-    });
+    //     scrollGallery.style.transform = `translateY(${offsetY}px)`;
+    // });
 
 });
