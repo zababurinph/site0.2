c19126f7cf4975d57a749feb195be73b2ef8377c
Tim Zababurin <raccoontd@mail.ru>
2022-02-04 20:46:25 +0300

    css-well-done_withoutScrollX

--- src/js/main.js
+++ src/js/main.js
@@ -12,28 +12,28 @@ let deltaH = 0;
 let deltaW = 0;
 
 window.addEventListener('load', () => {
-    
+
     scrollContainer.addEventListener('wheel', (evt) => {
         deltaW = scrollContainer.scrollWidth - document.documentElement.clientWidth;
         deltaH = scrollGallery.offsetHeight - document.documentElement.clientHeight;
         let offsetGallery = -(document.querySelector(".main").offsetWidth + document.querySelector(".taplink").offsetWidth + document.querySelector(".about").offsetWidth + 93);
 
         evt.preventDefault();
-        
-        if (scrollBeforeGallery || scrollAfterGallery) { offsetX -= evt.deltaY * deltaV;  }
+
+        if (scrollBeforeGallery || scrollAfterGallery) offsetX -= evt.deltaY * deltaV
         else offsetY -= evt.deltaY * deltaV;
-        
+
         if (offsetX > 0) offsetX = 0;
         if (offsetX < -deltaW) offsetX = -deltaW;
         if (offsetX < offsetGallery && scrollBeforeGallery) { scrollBeforeGallery = false; offsetX = offsetGallery };
         if (offsetX > offsetGallery && scrollAfterGallery) { scrollAfterGallery = false; offsetX = offsetGallery };
 
-        if (offsetY > 0 && !scrollBeforeGallery) {offsetY = 0; scrollBeforeGallery = true }
+        if (offsetY > 0 && !scrollBeforeGallery) { offsetY = 0; scrollBeforeGallery = true }
         if (offsetY < -deltaH && !scrollAfterGallery) { offsetY = -deltaH; scrollAfterGallery = true }
 
         scrollContainer.style.transform = `translateX(${offsetX}px)`;
         scrollGallery.style.transform = `translateY(${offsetY}px)`;
 
-        console.log(offsetX, offsetY, scrollBeforeGallery, scrollAfterGallery);
+        // console.log(offsetX, offsetY, scrollBeforeGallery, scrollAfterGallery);
     });
 });
